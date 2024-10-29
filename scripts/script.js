950064a2237fa5b69de7b21a17e7d5e6dc618d22
Lastered <expoexpo1020@gmail.com>
2024-10-28 17:49:06 -0700

    Added firebase basics
    added the basic stuff for firebase
    made firebase project
    added login functionality
    basically just everything from lab07

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,10 @@
-function sayHello() {
-    
+function logout() {
+    firebase.auth().signOut().then(() => {
+        // Sign-out successful.
+
+        
+        console.log("logging out user");
+      }).catch((error) => {
+        // An error happened.
+      });
 }
-//sayHello();
