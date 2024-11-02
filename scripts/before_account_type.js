1eb67775eab8b1b84233429f7cabf50c7e06e390
Lastered <expoexpo1020@gmail.com>
2024-11-02 13:58:58 -0700

    Added ability to change account type as either a recruiter or a job-seeker
    Once the user chooses an account type, they cannot change it.
    In firebase, this is stored as a boolean variable called `isRecruiter`

---
+++ scripts/before_account_type.js
@@ -0,0 +1,53 @@
+/*
+ * This script is used to update the isRecruiter field in the user's document in the Firestore database.
+ * A user cannot change their account type after they have created their account.
+ */
+
+function updateIsRecruiter(isRecruiter) {
+    return new Promise((resolve, reject) => {
+        firebase.auth().onAuthStateChanged(user => {
+            if (user) {
+                const userRef = db.collection("users").doc(user.uid);
+
+                userRef.get().then(doc => {
+                    if (doc.exists) {
+                        if (doc.data().isRecruiter != null) {
+                            console.log("isRecruiter field already exists.");
+                            reject("isRecruiter field already exists.");
+                            return;
+                        }
+                    }
+
+
+                    userRef.update({
+                        isRecruiter: isRecruiter
+                    }).then(() => {
+                        console.log("isRecruiter field updated successfully.");
+                        resolve();
+                    }).catch(error => {
+                        console.error("Error updating isRecruiter field: ", error);
+                        reject(error);
+                    });   
+                });
+
+            } else {
+                console.log("No user is logged in.");
+                reject("No user is logged in.");
+            }
+        });
+    });
+}
+
+
+
+document.getElementById("jobSeekerBtn").addEventListener("click", function() {
+    updateIsRecruiter(false).then(() => {
+        window.location.href = "main.html";
+    });
+});
+
+document.getElementById("recruiterBtn").addEventListener("click", function() {
+    updateIsRecruiter(true).then(() => {
+        window.location.href = "main.html";
+    });
+});
