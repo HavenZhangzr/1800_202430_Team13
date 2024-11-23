91b980c38f9d5db1ad0469d080a0b773654a6840
Szymon <szymonzemojtel@gmail.com>
2024-11-23 14:05:25 -0800

    Fixed signing out, keeping a logged-in user navbar bug

--- scripts/script.js
+++ scripts/script.js
@@ -5,9 +5,27 @@ function logout() {
   firebase.auth().signOut().then(() => {
       // Sign-out successful.
       console.log("logging out user");
+
+      // removing teh user from the local storage
+      localStorage.removeItem("user");
+      // userid too
+      localStorage.removeItem("userid");
+
+      //clear all memory
+      localStorage.clear();
+
+      // without this check, it just loops over and over again 🥲
+      console.log(window.location.href);
+
+      const pathname = window.location.pathname;
+      const lastPart = pathname.split('/').pop();
+
+      if (lastPart != "index.html") {
+        window.location.href = "index.html";
+      }
+
+
     }).catch((error) => {
       // An error happened.
     });
 }
-
-document.getElementById("logout").addEventListener("click", logout);
